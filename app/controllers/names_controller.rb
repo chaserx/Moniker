e78862de01f74e13145350d9e96ea0527a65423f
Chase Southard <chase.southard@gmail.com>
2009-11-18 22:06:21 -0500

    more work on the index action of the names controller

---
+++ app/controllers/names_controller.rb
@@ -0,0 +1,11 @@
+class NamesController < ApplicationController
+  def index
+    #@names = Name.all
+    if params[:namelist]
+      inputnames = params[:namelist]
+      @names = inputnames.to_s.split.each{|e|e.capitalize}
+    else
+      @names = "No Names"
+    end
+  end
+end
