a33cd9141896b5b6d016bf92be6015a6ba0aaae4
Chase Southard <chase.southard@gmail.com>
2009-11-18 22:00:54 -0500

    rethinking how this should work. made a names controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :names
+
   map.resources :monikers
 
   # The priority is based upon order of creation: first created -> highest priority.
