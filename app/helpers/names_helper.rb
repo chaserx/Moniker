167103cb3e7df89a294479138f1effa3929636d7
Chase Southard <chase.southard@gmail.com>
2009-11-22 21:09:23 -0500

    index tweaks with image helper

--- app/helpers/names_helper.rb
+++ app/helpers/names_helper.rb
@@ -1,5 +1,13 @@
 module NamesHelper
   
-  
+  def generate_gender_symbol(gender)
+    if gender == 'male'
+      image_tag("male.png", :size => "16x16", :alt => "Male")
+    elsif gender == 'female'
+      image_tag("female.png", :size => "16x16", :alt => "Female")
+    else
+      image_tag("error.png", :size => "16x16", :alt => "error")
+    end
+  end
   
 end
